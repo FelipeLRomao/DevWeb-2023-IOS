3f6a052e85a59db33d6cc06251190aa4474c5104
FelipeRomaoDev <felipe.romao@outlook.com>
2023-05-24 21:38:58 -0300

    Atualizando arquivos curso

--- workspace/JS/Aula JS 11 - JavaScript HTML DOM – Parte 01/main.js
+++ workspace/JS/Aula JS 11 - JavaScript HTML DOM – Parte 01/main.js
@@ -1,2 +1,26 @@
-window.alert('Olá alunos');
-alert('Olá alunos2');
+//window.alert('Olá alunos');
+//alert('Olá alunos2');
+
+// JavaScript e DOM
+// objeto document
+console.log(document);
+
+// Acessar informações
+console.clear();
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+
+// Alterar o título
+document.title = 'socorro Deus';
+// Criar um elemento <h1>
+let heading = document.createElement('H1');
+heading.innerHTML = 'Olá alunos IOS!';
+document.body.appendChild(heading);
+
+
+document.createElement('BUTTON');
+document.createElement('p');
+document.createElement('img');
